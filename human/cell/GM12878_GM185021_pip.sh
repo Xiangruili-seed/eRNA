e3d8ce719a94eed506f81e58e353894bd10208b4
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-05-14 11:41:18 +0800

    Create GM12878_GM185021_pip.sh

---
+++ human/cell/GM12878_GM185021_pip.sh
@@ -0,0 +1,25 @@
+#####example for gm12878_gm185021
+
+#########step 1: use seruat to filter data
+################dir/barcodes.tsv dir/genes.tsv dir/matrix.mtx dir/data/bam
+source activate r4-base
+#########get fantom enhancer
+nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix GM185021_GM12878 &
+nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/12878/outs/filtered_feature_bc_matrix GM12878 &
+nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/185021/outs/filtered_feature_bc_matrix GM185021 &
+
+
+wait;
+##############################run split bam by barcodes and get signal for positive enhancer 
+nohup bash ~/eRNA/12878/step_bam.sh ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix/data/possorted_genome_bam.bam  &
+nohup bash ~/eRNA/12878/step_bam.sh ~/eRNA/12878/outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed ~/eRNA/12878/outs/filtered_feature_bc_matrix/data/possorted_genome_bam.bam &
+nohup bash ~/eRNA/12878/step_bam.sh ~/eRNA/185021/outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed ~/eRNA/185021/outs/filtered_feature_bc_matrix/data/possorted_genome_bam.bam &
+wait;
+
+
+source activate r-env
+
+nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix &
+nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/12878/outs/filtered_feature_bc_matrix &
+nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/185021/outs/filtered_feature_bc_matrix &
+
