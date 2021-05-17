af5ebd514593887754cbd3cc5c5303732a3f011b
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-05-17 13:30:22 +0800

    Update test_eRNA.sh

--- mouse/test_eRNA.sh
+++ mouse/test_eRNA.sh
@@ -8,18 +8,18 @@ all=$4
 	nohup bwtool agg 300:300 ${bed} ${dir}/${annotation}/data/filtered_reverse.bigWig ${dir}/${annotation}/data/fpos_plus.txt &
 	nohup bwtool agg 300:300 ${bed} ${dir}/${annotation}/data/filtered_forward.bigWig  ${dir}/${annotation}/data/fpos_minus.txt &
 wait;
-	nohup Rscript ~/eRNA/script/RNA.R ${dir}/${annotation}/data/ ${annotation} &
+	nohup Rscript ~/eRNA/script/RNA.R ${dir}/${annotation}/data/ &
 #################chip signal
 
-ls -R ${dir}/chip/*.bigWig > ${dir}/chip/${annotation}.txt
-n=$(cat ${dir}/chip/${annotation}.txt|wc -l)
+ls -R ${dir}/chip/*.bigWig > ${dir}/chip/annotation.txt
+n=$(cat ${dir}/chip/annotation.txt|wc -l)
 
 
 
 cat ${all}|awk '{print $1"\t"$2"\t"$3}' >${all}.bed
 for k in $(seq $n)\t
 do
-    peakFile=$(cat ${dir}/chip/${annotation}.txt| awk -F "\t" '{if (NR == '$k') print $1}')
+    peakFile=$(cat ${dir}/chip/annotation.txt| awk -F "\t" '{if (NR == '$k') print $1}')
     bwtool agg 500:500 ${bed} $peakFile $peakFile.pos.txt
     bwtool agg 500:500 ${all}.bed $peakFile $peakFile.txt
 
@@ -29,7 +29,7 @@ wait;
 
 for k in $(seq $n)
 do
-    peakFile=$(cat ${dir}/chip/${annotation}.txt| awk -F "\t" '{if (NR == '$k') print $1}')
+    peakFile=$(cat ${dir}/chip/annotation.txt| awk -F "\t" '{if (NR == '$k') print $1}')
     nohup Rscript ~/eRNA/script/chip_t.R $peakFile pos &
 
 
