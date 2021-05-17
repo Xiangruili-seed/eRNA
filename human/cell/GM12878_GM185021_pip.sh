6bd99dfc7c9db93bece5c30673f85732bf8e3bd3
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-05-17 16:31:32 +0800

    Update GM12878_GM185021_pip.sh

--- human/cell/GM12878_GM185021_pip.sh
+++ human/cell/GM12878_GM185021_pip.sh
@@ -31,12 +31,10 @@ nohup ~/eRNA/cellranger-4.0.0/cellranger count --id=185021_GM12878 --transcripto
 #########step 1: use seruat to filter data
 ################dir/barcodes.tsv dir/genes.tsv dir/matrix.mtx dir/data/bam
 source activate r4-base
-#########get fantom enhancer
-nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix GM185021_GM12878 &
+#########filter data
+##nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix GM185021_GM12878 &
 nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/12878/outs/filtered_feature_bc_matrix GM12878 &
 nohup Rscript ~/eRNA/12878/step1_filter.R ~/eRNA/185021/outs/filtered_feature_bc_matrix GM185021 &
-
-
 wait;
 ##############################run split bam by barcodes and get signal for positive enhancer 
 #nohup bash ~/eRNA/12878/step_bam.sh ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix/data/possorted_genome_bam.bam  &
@@ -44,12 +42,11 @@ nohup bash ~/eRNA/12878/step_bam.sh ~/eRNA/12878/outs/filtered_feature_bc_matrix
 nohup bash ~/eRNA/12878/step_bam.sh ~/eRNA/185021/outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed ~/eRNA/185021/outs/filtered_feature_bc_matrix/data/possorted_genome_bam.bam &
 wait;
 
-
+#################get single cell reads for eRNA
 source activate r-env
-
 #nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/185021_GM12878/outs/filtered_feature_bc_matrix &
 nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/12878/outs/filtered_feature_bc_matrix &
 nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/185021/outs/filtered_feature_bc_matrix &
-
+##############get chip signal for eRNA
 nohup bash ~/eRNA/script/test_eRNA.sh ~/eRNA/12878/ ~/eRNA/12878/outs/filtered_feature_bc_matrix/data/f_dELS_reads.bed outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed &
 nohup bash ~/eRNA/script/test_eRNA.sh ~/eRNA/185021/ ~/eRNA/185021/outs/filtered_feature_bc_matrix/data/f_dELS_reads.bed outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed &
