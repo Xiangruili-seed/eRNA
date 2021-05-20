23f8d5714f2f6bf3e5e225dec9e575926ef80627
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-05-20 14:28:05 +0800

    Create cor_ne_eRNA.R

---
+++ human/cell/cor_ne_eRNA.R
@@ -0,0 +1,20 @@
+#~/eRNA/pbmcs/pos/top_0.05_cor.txt
+args<-commandArgs(T)
+
+dir<-args[1]
+load(paste(dir,'/.Rdata',sep = ""))
+#A Genome-wide Framework for Mapping Gene Regulation via Cellular Genetic Screens
+library("Nebulosa")
+library("Seurat")
+library("BiocFileCache")
+data<-read.table(paste(dir,"//top_0.05_cor_cut.txt",sep = ""),header=F,stringsAsFactors = F)
+
+pdf(file = paste(dir,"/Nebulosa_cut.pdf",sep = ""))
+
+for (i in 1:nrow(data)) {
+	plot<-plot_density(pbmc, as.character(data[i,1:2]), joint = TRUE,method = "wkde",pal = "viridis")
+	print(plot)
+
+}
+
+dev.off()
