32609f86a745dde914a0fb0b1a5ebf35436551ec
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-06-07 10:52:45 +0800

    Update GM12878_GM185021_pip.sh

--- human/cell/GM12878_GM185021_pip.sh
+++ human/cell/GM12878_GM185021_pip.sh
@@ -48,8 +48,8 @@ nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/185021_GM12878/outs/filtered_featu
 nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/12878/outs/filtered_feature_bc_matrix dELS &
 nohup bash ~/eRNA/12878/signal_eRNA.sh ~/eRNA/185021/outs/filtered_feature_bc_matrix dELS &
 ##############get chip signal for eRNA
-nohup bash ~/eRNA/script/test_eRNA.sh ~/eRNA/12878/ ~/eRNA/12878/outs/filtered_feature_bc_matrix/data/f_dELS_reads.bed outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed &
-nohup bash ~/eRNA/script/test_eRNA.sh ~/eRNA/185021/ ~/eRNA/185021/outs/filtered_feature_bc_matrix/data/f_dELS_reads.bed outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed &
+nohup bash ~/eRNA/script/test_eRNA.sh ~/eRNA/12878/ ~/eRNA/12878/outs/filtered_feature_bc_matrix/data/f_dELS_reads.bed outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed dELS &
+nohup bash ~/eRNA/script/test_eRNA.sh ~/eRNA/185021/ ~/eRNA/185021/outs/filtered_feature_bc_matrix/data/f_dELS_reads.bed outs/filtered_feature_bc_matrix /data/tusers/lixiangr/eRNA/single-cell/hg38/PBMCs/data/pos_dELS.bed dELS &
 ########get eRNA matrix
 nohup bash ~/eRNA/script/matrix_eRNA.sh ~/eRNA/12878/outs/filtered_feature_bc_matrix &
 nohup bash ~/eRNA/script/matrix_eRNA.sh ~/eRNA/185021/outs/filtered_feature_bc_matrix &
