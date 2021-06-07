a684826770cf49e0379a3378168b07ac113d4a83
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-06-07 10:51:39 +0800

    Update test_eRNA.sh

--- human/cell/test_eRNA.sh
+++ human/cell/test_eRNA.sh
@@ -2,13 +2,13 @@ dir=$1
 bed=$2
 annotation=$3
 all=$4
-
+type=$5
 ###########################get rna-seq signal
 	nohup bwtool agg 300:300 ${bed} ${dir}/${annotation}/data/filtered.bigWig ${dir}/${annotation}/data/fpos.txt &
 	nohup bwtool agg 300:300 ${bed} ${dir}/${annotation}/data/filtered_reverse.bigWig ${dir}/${annotation}/data/fpos_plus.txt &
 	nohup bwtool agg 300:300 ${bed} ${dir}/${annotation}/data/filtered_forward.bigWig  ${dir}/${annotation}/data/fpos_minus.txt &
 wait;
-	nohup Rscript ~/eRNA/script/RNA.R ${dir}/${annotation}/data/ &
+	nohup Rscript ~/eRNA/script/RNA.R ${dir}/${annotation}/data/ $type &
 #################chip signal
 
 ls -R ${dir}/chip/*.bigWig > ${dir}/chip/annotation.txt
@@ -30,7 +30,7 @@ wait;
 for k in $(seq $n)
 do
     peakFile=$(cat ${dir}/chip/annotation.txt| awk -F "\t" '{if (NR == '$k') print $1}')
-    nohup Rscript ~/eRNA/script/chip_t.R $peakFile pos &
+    nohup Rscript ~/eRNA/script/chip_t.R $peakFile pos $type &
 
 
 done
