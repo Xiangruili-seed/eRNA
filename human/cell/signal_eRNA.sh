eb0d3f6a86991fa099c2d7368fee6ab26a901576
Xiangruili-seed <66310256+Xiangruili-seed@users.noreply.github.com>
2021-06-07 10:12:05 +0800

    Create signal_eRNA.sh

--- human/cell/signal_eRNA.sh
+++ human/cell/signal_eRNA.sh
@@ -1,18 +1,20 @@
 ########################################signal_eRNA.sh
 dir=$1
+type=$2
+
 source activate r-env
-nohup Rscript ~/eRNA/script/cut.R ${dir}/data/f_dELS_reads.txt &
+nohup Rscript ~/eRNA/script/cut.R ${dir}/data/f_${type}_reads.txt &
 
 wait;
 #########cut dot
 
-	d=$(cat ${dir}/data/f_dELS_reads.txt_dot.txt|awk '{print $0}')
-	awk '{if($7 >n)  print $1"\t"$2"\t"$3"\t"$4"\t"$7"\t"$6}' n=$d ${dir}/data/f_dELS_reads.txt >${dir}/data/f_dELS_reads.bed
+	d=$(cat ${dir}/data/f_${type}_reads.txt_dot.txt|awk '{print $0}')
+	awk '{if($7 >n)  print $1"\t"$2"\t"$3"\t"$4"\t"$7"\t"$6}' n=$d ${dir}/data/f_${type}_reads.txt >${dir}/data/f_${type}_reads.bed
 
 wait;
 
 
-mkdir -p ${dir}/data/reads
+mkdir -p ${dir}/data/reads_${type}
 
 n1=$(cat ${dir}/data/f_barcode.txt|wc -l)
 n=$(cat ${dir}/data/f_barcode.txt ${dir}/data/f_barcode.txt|wc -l)
@@ -24,13 +26,13 @@ do
 	echo "nohup ~/anaconda2/envs/r-env/bin/samtools index -@ 48 ${dir}/data/splits/CB_$q.bam &" >>${dir}/data/reads.sh
 done
 
-rm ${dir}/data/reads/reads.sh
+rm ${dir}/data/reads_${type}/reads.sh
 
-echo "source activate r-env">>${dir}/data/reads/reads.sh
+echo "source activate r-env">>${dir}/data/reads_${type}/reads.sh
 
 for q in $(seq $n1)
 do
-	echo "nohup bedtools multicov -bams ${dir}/data/splits/CB_$q.bam -bed ${dir}/data/f_dELS_reads.bed > ${dir}/data/reads/reads_$q.txt &" >>${dir}/data/reads/reads.sh
+	echo "nohup bedtools multicov -bams ${dir}/data/splits/CB_$q.bam -bed ${dir}/data/f_${type}_reads.bed > ${dir}/data/reads_${type}/reads_$q.txt &" >>${dir}/data/reads_${type}/reads.sh
 
 done
 
@@ -45,18 +47,18 @@ done
 
 for i in $(seq 1 5 $n)
 do
-	sed -i ''"$i"'i wait;' ${dir}/data/reads/reads.sh
+	sed -i ''"$i"'i wait;' ${dir}/data/reads_${type}/reads.sh
 done
 
 
 
 wait;
-echo "wait;">>${dir}/data/reads/reads.sh
+echo "wait;">>${dir}/data/reads_${type}/reads.sh
 echo "wait;">>${dir}/data/reads.sh
 
 ulimit -n 20000
 nohup bash ${dir}/data/reads.sh &
 wait;
-nohup bash ${dir}/data/reads/reads.sh &
+nohup bash ${dir}/data/reads_${type}/reads.sh &
 wait;
 
